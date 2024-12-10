0888ef13e36a6650e5be9f95dafb7b17e310c7a5
raihanshi18 <raihanshiddiq18@gmail.com>
2024-12-11 06:24:28 +0700

    Figma-React

--- src/components/Entertain.jsx
+++ src/components/Entertain.jsx
@@ -7,8 +7,8 @@ const Entertain = () => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#272626]">
       <h1 className="text-5xl font-bold p-9 text-white">Entertaiment</h1>
       <div className="flex flex-wrap justify-center gap-6">
-        <img src={en1} alt="riot games" width={750} className=" cursor-pointer hover:outline outline-4 outline-white rounded-md"/>
-        <img src={en2} alt="riot games" width={750} className=" cursor-pointer hover:outline outline-4 outline-white rounded-md"/>
+        <img src={en1} alt="riot games" width={600} className=" cursor-pointer hover:outline outline-4 outline-white rounded-md"/>
+        <img src={en2} alt="riot games" width={600} className=" cursor-pointer hover:outline outline-4 outline-white rounded-md"/>
       </div>
     </div>
   )
