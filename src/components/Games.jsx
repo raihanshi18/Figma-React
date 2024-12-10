b00184eaee05b5039cdcc3bb82449e32050d2790
raihanshi18 <raihanshiddiq18@gmail.com>
2024-12-11 06:14:21 +0700

    Figma-React

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import item1 from "../assets/Item.png";
+import itemno1 from "../assets/Item1.png"
 import item2 from "../assets/Item (1).png";
 import item3 from "../assets/Item (2).png";
 import item4 from "../assets/Item (3).png";
@@ -9,7 +9,7 @@ const Games = () => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#272626]">
       <h1 className="text-5xl font-bold p-9 text-white">Games</h1>
       <div className="flex flex-wrap justify-center gap-6 ">
-        <img src={item1} alt="riot games" width={800} className=" cursor-pointer hover:outline outline-4 outline-white rounded-md"/>
+        <img src={itemno1} alt="riot games" width={800} className=" cursor-pointer hover:outline outline-4 outline-white rounded-md"/>
         <img src={item2} alt="riot games" width={800} className=" cursor-pointer hover:outline outline-4 outline-white rounded-md"/>
         <img src={item3} alt="riot games" width={800} className=" cursor-pointer hover:outline outline-4 outline-white rounded-md"/>
         <img src={item4} alt="riot games" width={800} className=" cursor-pointer hover:outline outline-4 outline-white rounded-md"/>
